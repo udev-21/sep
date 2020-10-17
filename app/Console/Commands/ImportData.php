f86119aabbf9d94133628fb205bafb4d3e61c3c7
Sharofbek Xudoyberdiyev <udev21@gmail.com>
2020-10-17 08:33:58 +0300

    fixing command name

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use App\Stat;
 use Illuminate\Support\Facades\DB;
-class ImportTable extends Command
+class ImportData extends Command
 {
     /**
      * The name and signature of the console command.
@@ -21,7 +21,7 @@ class ImportTable extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Importing data';
 
     /**
      * Create a new command instance.
@@ -46,32 +46,42 @@ class ImportTable extends Command
     }
 
     public function update(){
+        echo "Update has begun\n";
+        echo "Downloading...\n";
         DB::statement('truncate passports');
-        $last_stat = new Stat();
-        // $link = "https://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2";
-        // $file = file_get_contents($link);
-        // if (!$file){
-        //         $file = file_get_contents($link);
-        //         if(!$file){
-        //             $last_stat->save();
-        //             return ['ok'=>false, "error"=>"File has not donwloaded :("];
-        //         }
-        // }
-        $last_stat->downloaded = true;
+        // $last_stat = new Stat();
+        $link = "https://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2";
+        $file = file_get_contents($link);
+        if (!$file){
+                $file = file_get_contents($link);
+                if(!$file){
+                    // $last_stat->save();
+                    return ['ok'=>false, "error"=>"File has not donwloaded :("];
+                }
+        }
+        // $last_stat->downloaded = true;
+        echo "Downloading compeleted !\n";
+            
         $fname = "dump/data.".date("d.m.Y").".csv.bz2";
-        //$res = Storage::put($fname, $file,'public');
-        if($res = true){
+        $res = Storage::put($fname, $file,'public');
+        echo "File stored as $fname to storage/dump\n";
+        if($res){
             $storagePath = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix();
             $path = $storagePath.$fname;
-            //exec("bunzip2 -dk $path", $r, $code);
+            echo "Unzipping...\n";
+        
+            exec("bunzip2 -d $path", $r, $code);
+            echo "Unzipping compeleted !\n";
+            echo "Reading data and updating DB...\n";
             $code = 0;
             if($code === 0){
-                $last_stat->unzipped = true;
+                //$last_stat->unzipped = true;
                 if (($handle = fopen($storagePath."dump/data.".date("d.m.Y").".csv", "r")) !== FALSE) {
                     $datas  = [];
                     fgetcsv($handle,100,',');
                     $start = microtime(true);
                     $ic = 0;
+                    $icc = 1;
                     DB::beginTransaction();
                     while (($data = fgetcsv($handle, 100, ",")) !== FALSE) {
                         $datas[] = [
@@ -85,27 +95,28 @@ class ImportTable extends Command
                             if($ic % 50 == 0 ){
                                 DB::commit();
                                 DB::beginTransaction();
-                                // DB::table('ttest')->insert([intval(time()) - intval($start)]);
-                                echo "250K records inserted in  ". (microtime(true) - $start) ."  seconds\n";
+                                echo "250K * $icc records inserted in  ". (microtime(true) - $start) ."  seconds\n";
                                 $start = microtime(true);
+                                $icc++;
                             }
                         }
                     }
+                    $res = DB::table('passports')->insertOrIgnore($datas);
                     DB::commit();
                     
                     // dump($datas);
                     fclose($handle);
-                    // $res = DB::table('passports')->insertOrIgnore($datas);
                     // dd($res);
                 }
 
-                echo "everything is ok;";
+                echo "Successfully updated;";
+                // echo "everything is ok;";
             }else{
 
             }
-            $last_stat->save();
+            // $last_stat->save();
             return ['ok'=>true, "error"=>""];
         }
-        $last_stat->save();
+        // $last_stat->save();
     }
 }
